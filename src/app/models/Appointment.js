a50937519f00e41a8d28005632a11428fc42ebfc
Ronaldo <rfeitosa@ciandt.com>
2019-08-12 21:48:32 -0300

    Appointment service

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -16,8 +16,8 @@ class Appointment extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.user, { foreignKey: 'user_id', as: 'user' });
-    this.belongsTo(models.user, { foreignKey: 'provider_id', as: 'provider' });
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
   }
 }
 
